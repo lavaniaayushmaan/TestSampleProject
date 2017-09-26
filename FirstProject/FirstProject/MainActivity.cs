6ade4e37b3baf7c46dac31695d877c60bbb6ce75
ayushmaan <ayushmaan.lavania@gaussnetworks.com>
2017-09-26 16:19:51 +0530

    commit

--- FirstProject/FirstProject/MainActivity.cs
+++ FirstProject/FirstProject/MainActivity.cs
@@ -12,7 +12,8 @@ namespace FirstProject
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+             SetContentView (Resource.Layout.Main);
+
         }
     }
 }
